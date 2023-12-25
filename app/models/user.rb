cbaf800daf966a845b6041a7d621aa6059cde82a
Mika Higashizima <144198758+higashizima@users.noreply.github.com>
2023-12-25 16:46:31 +0000

    no imageファイル変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,8 +43,8 @@ class User < ApplicationRecord
   # プロフィール画像取得
   def get_profile_image(width, height)
   unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/test_cow.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'test_cow.jpg', content_type: 'image/j')
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
   end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
